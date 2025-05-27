2b2891ad3806bac31041cf79cf918981e4e78b0c
zhangxinhui <xinhui.zhang@wuitu.com>
2025-05-27 15:12:44 +0800

    style: 界面样式调整;

--- src/app/components/AiChatContent.tsx
+++ src/app/components/AiChatContent.tsx
@@ -62,8 +62,8 @@ const AiChatContent: React.FC = () => {
   const mainRender = useMemo(() => {
     if (items.length === 0) {
       return (
-        <ScrollArea className="w-full flex-1 rounded-md">
-          <div className="bg-[#6678CE] p-4">
+        <ScrollArea className="w-full flex-1">
+          <div className="rounded-2xl bg-[#6678CE] p-4">
             <p>你好，我是你的私人健身教练</p>
             <p>你好，我是你的私人健身教练</p>
             <p>你好，我是你的私人健身教练</p>
@@ -81,7 +81,7 @@ const AiChatContent: React.FC = () => {
     <>
       {mainRender}
       <div className="fixed bottom-[80px] left-0 z-10 flex w-full items-center justify-center bg-[#374887] py-2 text-center text-xs text-white">
-        <Image src="/logo.png" alt="logo" width={100} height={100} className="mr-2" />
+        <Image src="/logo.png" alt="logo" width={100} height={14} className="mr-2" />
         <div className="mr-4 ml-2 h-6 w-[1px] bg-white/50"></div>
         <span>深度思考 (DeepSeek R1)</span>
       </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import AiChatContent from './components/AiChatContent';
 
 export default function HomePage() {
   return (
-    <main className="relative flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[rgba(80,88,126,0.94)] to-[rgba(102,120,206,0.94)] text-white">
+    <main className="relative flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#50587E] to-[rgba(102,120,206,0.94)] text-white">
       <div className="container flex h-[100vh] flex-col items-center p-4">
         <AiChatContent />
       </div>
