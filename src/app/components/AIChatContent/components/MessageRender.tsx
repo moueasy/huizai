8dbd1c4bb45f0f6192632d5c528572bda5b1e1b9
zhangxinhui <xinhui.zhang@wuitu.com>
2025-06-17 10:29:43 +0800

    feat: message组件细节调整;

--- src/app/components/AIChatContent/components/MessageRender.tsx
+++ src/app/components/AIChatContent/components/MessageRender.tsx
@@ -30,9 +30,9 @@ const MessageRender = ({
         <Accordion type="single" collapsible defaultValue="thinking">
           <AccordionItem value="thinking">
             <AccordionTrigger className="rounded-t-2xl bg-[#374887] px-4 py-2">
-              已深度思考（用时{content.thinkingTime}秒）-{appVersionCode.current}-{parseInt(minAppVersionCode)}
+              已深度思考（用时{content.thinkingTime}秒
             </AccordionTrigger>
-            <AccordionContent className="bg-[#374079] px-4 py-2 text-[rgba(255,255,255,0.8)]">
+            <AccordionContent className="bg-[#374079] px-4 py-2 text-justify text-[rgba(255,255,255,0.8)]">
               {content.reasoningContentText}
             </AccordionContent>
           </AccordionItem>
